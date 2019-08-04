11aeea6a4a41c4c9a77919ebc48dd7035de27c00
mpan <mike.pan@wework.com>
2019-08-04 20:45:34 +0800

    build

---
+++ dist/precache-manifest.c4ee17d249320ea13d57397f4fd3ffa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a49a9c46a568a9f3b18271f1908b3c7",
+    "url": "/react-test/dist/index.html"
+  },
+  {
+    "revision": "512929e5e08494ab61e3",
+    "url": "/react-test/dist/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/react-test/dist/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "512929e5e08494ab61e3",
+    "url": "/react-test/dist/static/js/main.52e7ccdb.chunk.js"
+  },
+  {
+    "revision": "beb2322006d10dc60c38",
+    "url": "/react-test/dist/static/js/runtime~main.1ab974d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/dist/static/media/logo.5d5d9eef.svg"
+  }
+]);
